155485b5de083f8272a84d28fe54183dbd9db560
Alex Prugh <prugh.alexander@gmail.com>
2025-09-08 18:56:39 -0400

    nav bar has correct items now

--- decidr-web/src/app/app.ts
+++ decidr-web/src/app/app.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,           
-  imports: [RouterOutlet, CommonModule, MatToolbarModule, RouterModule],   
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, MatToolbarModule, RouterModule],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss'] 
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = 'Decidr';
+
+  constructor(private router: Router) { }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('jwt');
+  }
+
+  logout(): void {
+    localStorage.removeItem('jwt');
+    this.router.navigate(['/login']);
+  }
 }
